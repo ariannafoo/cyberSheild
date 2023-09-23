ddc3712fa8d11e3e2aeca9abf1361641cce98363
Arianna Foo <fooar@sheridancollege.ca>
2023-09-23 12:20:20 -0400

    Created HomeView

---
+++ CyberShield/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  CyberShield
+//
+//  Created by Arianna Foo on 2023-09-23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
