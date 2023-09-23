80a7f8c589bfe1a48325a8fb57b5f4cde097e24a
Arianna Foo <fooar@sheridancollege.ca>
2023-09-23 12:02:18 -0400

    Initial Commit

---
+++ CyberShield/CyberShieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyberShieldApp.swift
+//  CyberShield
+//
+//  Created by Arianna Foo on 2023-09-23.
+//
+
+import SwiftUI
+
+@main
+struct CyberShieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
